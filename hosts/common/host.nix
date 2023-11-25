980364b8e70c11445869f84893a0f4d6d28d3cbc
pengiie <nathan@pengie.dev>
2023-11-25 03:16:57 +0000

    Nix on the desktop

--- hosts/common/host.nix
+++ hosts/common/host.nix
@@ -2,57 +2,15 @@
 let
   inherit (lib) mkOption types;
 in {
+  imports = [
+    ./monitors.nix
+  ];
+
   options = {
     host = {
       name = mkOption {
         type = types.str;
         description = "The hostname of the current machine.";
-      };
-      monitors = mkOption {
-        type = types.listOf (types.submodule {
-          options = {
-            name = mkOption {
-              type = types.str;
-              description = "The name of the monitor.";
-            };
-            resolution = {
-              width = mkOption {
-                type = types.int;
-                default = 1920;
-                description = "The width of the monitor.";
-              };
-              height = mkOption {
-                type = types.int;
-                default = 1080;
-                description = "The height of the monitor.";
-              };
-            };
-            refreshRate = mkOption {
-              type = types.int;
-              default = 60;
-              description = "The refresh rate of the monitor.";
-            };
-            position = {
-              x = mkOption {
-                type = types.int;
-                default = 0;
-                description = "The x position of the monitor.";
-              };
-              y = mkOption {
-                type = types.int;
-                default = 0;
-                description = "The y position of the monitor.";
-              };
-            };
-            scale = mkOption {
-              type = types.float;
-              default = 1.0;
-              description = "The scale of the monitor.";
-            };
-          };
-        });
-        default = [];
-        description = "A list of monitors to use for the host.";
       };
     };
   };
