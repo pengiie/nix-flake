52dc0713f8b218e61b8b65cc79fd1fca77fa32a0
pengiie <nathan@pengie.dev>
2023-11-28 07:14:34 +0000

    the laptop can ssh, no one else.

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -5,6 +5,15 @@
     ./hardware.nix
     ./host.nix
     ./nvidia.nix
-    ./samba.nix
+  ];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false; 
+  };
+
+  users.users."nathan".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFrpkAQpZDGEQ7wK0k/HqMs/5pUdR5ZTsRLRpEg1lBFp nathan"
   ];
 }
